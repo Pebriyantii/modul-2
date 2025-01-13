c4db99b870d0cce6900d7451bb7e5c551dc11f6f
Pebriyantii <Pebriyanti1802@gmail.com>
2025-01-13 13:48:57 +0700

    Add files via upload

---
+++ modul 2 tugas mandiri 2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define PI 3.14159 
+
+int main() {
+    float diameter = 15.0; 
+    float jari_jari = diameter / 2.0;
+    float volume;
+
+    volume = (4.0 / 3.0) * PI * jari_jari * jari_jari * jari_jari;
+
+    printf("Volume bola dengan diameter %.2f cm adalah %.2f cm^3\n", diameter, volume);
+
+    return 0;
+}
+
